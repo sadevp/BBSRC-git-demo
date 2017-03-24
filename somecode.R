83a54a0831274e5ade160911283c2707d22b43a9
Sadev Parikh <SSP@DN0a23312d.SUNet>
2017-03-24 12:41:21 -0700

    adding lm

--- somecode.R
+++ somecode.R
@@ -1,4 +1,7 @@
-#R code
+# R code
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
                header=TRUE,sep=',')
 head(df)
+# R code
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
